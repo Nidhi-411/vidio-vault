b45041e00509d0ace236ab0eb6a66dbb13e2629f
Nidhi Yadav <shub01690@gmail.com>
2024-07-02 23:13:57 +0530

    .

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,92 +1,101 @@
- 
- import { useDispatch } from "react-redux"
-
- import React, { useEffect, useState } from "react";
- import {toggleMenu} from "../utils/appSlice"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 import { YOUTUBE_SEARCH_API } from "../utils/Constants";
- const Head  = ()=>{ 
+import { cacheResults } from "../utils/searchSlice";
 
-   const [searchQuery, setSearchQuery] = useState("");
-   //console.log(searchQuery)
-   const [suggestions, setSuggestions] = useState([]);
+const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
 
-   useEffect(()=>{
-       //console.log( searchQuery);
+  const searchCache = useSelector((store) => store.search);
+  const dispatch = useDispatch();
 
-      // make an api call after every key press 
-      // but if the difference between 2 api calls is < 200 ms
-      // decline the api call 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (searchQuery && searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else if (searchQuery) {
+        getSearchSuggestions();
+      }
+    }, 200);
 
-      const timer = setTimeout(()=>getSearchSuggestions() , 200);
-       
-      return()=>{
-         clearTimeout(timer);
-      } 
-   },[searchQuery])
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery, searchCache]);
 
-   const getSearchSuggestions = async()=>{
-
-      const data = await fetch(YOUTUBE_SEARCH_API+searchQuery);
-      const json = await data.json();
-       console.log(json);
+  const getSearchSuggestions = async () => {
+    try {
+      const response = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+      const json = await response.json();
       setSuggestions(json[1]);
 
-   }
-     
-      const dispatch = useDispatch();
-     const handleToggelMenu = ()=>{
-        // dispatch an action
-        dispatch(toggleMenu());
-     }
+      dispatch(
+        cacheResults({
+          [searchQuery]: json[1],
+        })
+      );
+    } catch (error) {
+      console.error("Failed to fetch search suggestions:", error);
+    }
+  };
 
-    return (
-        <div className="grid grid-flow-col border-b-2 shadow-xl py-2 ">
-         <div className=" flex col-span-1">
-          <img
-          onClick={()=> handleToggelMenu()}
+  const handleToggleMenu = () => {
+    dispatch(toggleMenu());
+  };
+
+  return (
+    <div className="grid grid-flow-col border-b-2 shadow-xl py-2">
+      <div className="flex col-span-1">
+        <img
+          onClick={handleToggleMenu}
           className="h-11 mx-2 cursor-pointer"
-          alt="menu" 
+          alt="menu"
           src="https://static.vecteezy.com/system/resources/thumbnails/001/500/312/small/bullet-menu-icon-free-vector.jpg"
-          />
-          <img 
-          className="h-12 mx-2"  
-          alt="logo" 
-         src="https://cdn.pixabay.com/photo/2020/11/01/04/17/youtube-5702828_1280.png"
-         />
-         </div>
+        />
+        <img
+          className="h-12 mx-2"
+          alt="logo"
+          src="https://cdn.pixabay.com/photo/2020/11/01/04/17/youtube-5702828_1280.png"
+        />
+      </div>
 
-         <div className="col-span-10 my-3 px-16">
-
-         <input type="text" className="border border-gray-500 w-1/2 rounded-l-full p-1 " 
-           value={searchQuery}
-           onChange={(e) => setSearchQuery(e.target.value)}
+      <div className="col-span-10 my-3 px-16">
+        <input
+          type="text"
+          className="border border-gray-500 w-1/2 rounded-l-full p-1"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           onFocus={() => setShowSuggestions(true)}
           onBlur={() => setShowSuggestions(false)}
-
-         />
-         <button  className= "border bg-gray-300 border-gray-500 rounded-r-full p-1">&#x1F50D;</button>
-        {/* // </div> */}
-         {showSuggestions && (
+        />
+        <button className="border bg-gray-300 border-gray-500 rounded-r-full p-1">
+          &#x1F50D;
+        </button>
+        {showSuggestions && (
           <div className="fixed bg-white py-2 px-2 w-[28rem] shadow-lg rounded-lg border border-gray-100">
             <ul>
               {suggestions.map((s) => (
                 <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
                   🔍 {s}
-                </li> 
+                </li>
               ))}
             </ul>
           </div>
         )}
-       </div>
-         <div className=" col-span-1">
+      </div>
 
-          <img
+      <div className="col-span-1">
+        <img
           className="h-10 p-1"
-          alt="usericon" src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"/>
-         </div>
-        </div>
-    )
- }
+          alt="usericon"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+        />
+      </div>
+    </div>
+  );
+};
 
- export default Head;
+export default Head;
