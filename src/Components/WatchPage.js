7a10c2388fdda4555b0d95d01447ff3f110c4d91
Nidhi Yadav <shub01690@gmail.com>
2024-07-14 08:21:48 +0530

    N level nested comments

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentContainer";
 
 const WatchPage = ()=>{
 
@@ -16,6 +17,7 @@ const WatchPage = ()=>{
    // console.log(searchParams.get("v"));
 
     return (
+      <div className="flex flex-col w-full">
         <div className="p-5">
            < iframe width="800"
             height="400"
@@ -25,6 +27,8 @@ const WatchPage = ()=>{
                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
                referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen></iframe>
+        </div>
+        <CommentsContainer/>
         </div>
     )
 }
